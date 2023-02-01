7679d9b06e02f3dabb6e20665293963d756db4d9
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-02-01 23:06:35 +0100

    Backend Project Started - Initial Setup Complete

---
+++ src/main/java/com/dineshb/projects/usermgmt/portal/UserMgmtPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dineshb.projects.usermgmt.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dineshb/projects/usermgmt/portal/UserMgmtPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineshb.projects.usermgmt.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
