232f753ff635648fbc7355d70de777fc80170871
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-02-20 21:40:10 +0100

    Added HttpResponse Model.

---
+++ src/main/java/com/dineshb/projects/usermgmt/portal/response/HttpResponse.java
@@ -0,0 +1,17 @@
+package com.dineshb.projects.usermgmt.portal.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class HttpResponse {
+
+    private int statusCode;
+    private String reason;
+    private String message;
+    private HttpStatus httpStatus;
+}
