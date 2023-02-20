e9f63dfc18abcab7db10baf05e594d67d0f53693
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-02-20 12:13:17 +0100

    Added User Model.

---
+++ src/main/java/com/dineshb/projects/usermgmt/portal/model/User.java
@@ -0,0 +1,45 @@
+package com.dineshb.projects.usermgmt.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * User Model.
+ */
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joiningDate;
+
+    private String[] roles;
+    private String[] permissions;
+
+    private boolean isActive;
+    private boolean isLocked;
+}
