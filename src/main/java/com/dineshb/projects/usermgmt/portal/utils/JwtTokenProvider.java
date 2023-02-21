77cf68610f961e70ef607114cc1e23ac3458cd00
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-02-22 00:02:11 +0100

    JWT Implementation - 02

--- src/main/java/com/dineshb/projects/usermgmt/portal/utils/JwtTokenProvider.java
+++ src/main/java/com/dineshb/projects/usermgmt/portal/utils/JwtTokenProvider.java
@@ -1,13 +1,25 @@
 package com.dineshb.projects.usermgmt.portal.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.dineshb.projects.usermgmt.portal.model.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.*;
+import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.ISSUER;
+import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.AUDIENCE;
+import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.AUTHORITIES;
+import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.EXPIRATION_TIME;
+import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.CANNOT_VERIFY_TOKEN;
 
 public class JwtTokenProvider {
 
@@ -27,7 +39,29 @@ public class JwtTokenProvider {
     }
 
     private String[] fetchClaimsFromUser(UserPrincipal userPrincipal) {
-        return null;
+        return userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()).toArray(new String[0]);
+    }
+
+    public List<GrantedAuthority> getClaimsFromToken(String token) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        try {
+            JWTVerifier jwtVerifier = getVerifier();
+            grantedAuthorities = Arrays.stream(jwtVerifier.verify(token)
+                    .getClaim(AUTHORITIES).asArray(String.class))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        } catch (JWTVerificationException e) {
+            new JWTVerificationException(CANNOT_VERIFY_TOKEN);
+        }
+        return grantedAuthorities;
+    }
+
+    private JWTVerifier getVerifier() {
+        return JWT.require(Algorithm.HMAC512(jwtSecret))
+                .withIssuer(ISSUER).build();
     }
 
 
