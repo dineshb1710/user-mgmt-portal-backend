de78c756748be0634edd2c7fd6f0f0193d81f4bf
Dinesh Kumar Bhardwaj <dinesh.bhardwaj1710@gmail.com>
2023-02-22 00:23:27 +0100

    JWT Implementation - 03 [Finished]

--- src/main/java/com/dineshb/projects/usermgmt/portal/utils/JwtTokenProvider.java
+++ src/main/java/com/dineshb/projects/usermgmt/portal/utils/JwtTokenProvider.java
@@ -5,10 +5,16 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.dineshb.projects.usermgmt.portal.model.security.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -21,6 +27,7 @@ import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.A
 import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.EXPIRATION_TIME;
 import static com.dineshb.projects.usermgmt.portal.constants.SecurityConstants.CANNOT_VERIFY_TOKEN;
 
+@Component
 public class JwtTokenProvider {
 
     @Value("${jwt-config.secret}")
@@ -64,5 +71,35 @@ public class JwtTokenProvider {
                 .withIssuer(ISSUER).build();
     }
 
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest httpServletRequest) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+        return usernamePasswordAuthenticationToken;
+    }
 
+    public boolean isValidToken(String username, String token) {
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(token);
+    }
+
+    public String getSubjectFromToken(String token) {
+        try {
+            return getVerifier()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(CANNOT_VERIFY_TOKEN);
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+            return getVerifier()
+                    .verify(token)
+                    .getExpiresAt()
+                    .before(new Date());
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(CANNOT_VERIFY_TOKEN);
+        }
+    }
 }
